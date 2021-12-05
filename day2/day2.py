c4afa274c35a04fd543ea6e22a8c1ac2df493619
Pelle Windestam <pelle@windestam.se>
2021-12-05 13:04:33 +0100

    Day 2 part 2 solved

--- day2/day2.py
+++ day2/day2.py
@@ -14,7 +14,23 @@ with open("input.txt") as f:
             vert -= change
         else:
             print("Invalid movement")
-    print("Horizontal position: %d, vertical position: %d, sum: %d" % (horz, vert, horz*vert))
+    print("Part 1: Horizontal position: %d, vertical position: %d, sum: %d" % (horz, vert, horz*vert))
     
+    aim = 0
+    vert = 0
+    horz = 0
+    for line in data:
+        direction = line.split()[0]
+        change = int(line.split()[1])
+        if direction == "forward":
+            horz += change
+            vert += change * aim
+        elif direction == "down":
+            aim += change 
+        elif direction == "up":
+            aim -= change
+        else:
+            print("Invalid movement")
         
+    print("Part 2: Horizontal position: %d, vertical position: %d, sum: %d" % (horz, vert, horz*vert))
 
